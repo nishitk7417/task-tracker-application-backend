1db90cc372987028f244bd88ecdff990c9d4053b
Nishit <nishitku123@gmail.com>
2025-04-29 00:19:02 +0530

    project setup

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+// import userRouter from './routes/user.routes.js';
+// import songRouter from './routes/song.routes.js';
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true, limit: '16kb'}))
+app.use(express.static('public'))
+app.use(cookieParser())
+
+//routes declaration
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/song",songRouter);
+
+export {app}
